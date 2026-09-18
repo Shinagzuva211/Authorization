9b669e877e4ccf44f1b244e0208f54d5f8ba5767
Shinagzuva211 <abdumalikmahmudbekov25@gmail.com>
2026-09-18 17:21:03 +0500

    chore: remove node_modules, dist, .env from git tracking

--- dist/middlewares/logger.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-declare const logger: (req: Request, res: Response, next: NextFunction) => void;
-export default logger;
-//# sourceMappingURL=logger.d.ts.map
--- dist/middlewares/validateCar.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-declare const validateCar: (req: Request, res: Response, next: NextFunction) => void;
-export default validateCar;
-//# sourceMappingURL=validateCar.d.ts.map
